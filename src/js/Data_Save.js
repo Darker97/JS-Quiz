45d5740536b9b1e794de478672ae3a212ab4e178
christian@baltzer.de <christian@baltzer.de>
2019-09-29 15:03:05 +0200

    Cookies_load

--- src/js/Data_Save.js
+++ src/js/Data_Save.js
@@ -1,9 +1,25 @@
-function Cookie_Save(params) {
-    
+function Cookie_Save(name, points) {
+
 }
 
 function Cookie_Load(params) {
-    
+  let arr = []
+
+  if (document.cookie.length === 0) {
+    arr.push('No one has played this game yet :/')
+    return arr
+  }
+  let cookie = document.cookie
+  cookie = cookie.split(';')
+  for (let i = 0; i < cookie.length; i++) {
+    let temp = new Object()
+
+    const string = cookie[i].split(',')
+    temp.name = string[0]
+    temp.points = string[1]
+
+    arr.push(temp)
+  }
 }
 
 function Cookie_Resolve(params) {
