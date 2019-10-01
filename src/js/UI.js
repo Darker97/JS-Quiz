7269d0566764bad2c2db3d039430b21881274767
christian@baltzer.de <christian@baltzer.de>
2019-10-01 11:05:56 +0200

    More Documentation

--- src/js/UI.js
+++ src/js/UI.js
@@ -1,16 +1,27 @@
 import * as connection from 'data_connection.js'
 
+/**
+ * returns a view of type artikel
+ */
 export function view () {
   const temp = document.createElement('artikel')
   return temp
 }
 
+/**
+ * returns button with a name
+ * @param {String} name
+ */
 export function button (name) {
   const temp = document.createElement('button')
-  temp.name = name
+  temp.innerText = name
   return temp
 }
 
+/**
+ * returns label with the text
+ * @param {String} text
+ */
 export function label (text) {
   const temp = document.createElement('p')
   temp.innerText = text
@@ -27,6 +38,11 @@ export function input (name) {
   return temp
 }
 
+/**
+ * returns a select Elements with the answers displayed
+ * @param {String} question
+ * @param {Array with Strings} answers
+ */
 export function multipleChoice (question, answers) {
   const arr = answers
   const temp = document.createElement('select')
@@ -41,11 +57,18 @@ export function multipleChoice (question, answers) {
   return temp
 }
 
-export function timer_New (time) {
+/**
+ * returns a timer with the ID: Clock
+ * @param {String} time
+ */
+export function timerNew (time) {
   // TODO: Timer UI
 }
-
-export function timer_Update (time) {
+/**
+ * updates the timer to the given Time
+ * @param {String} time
+ */
+export function timerUpdate (time) {
   // TODO: Timer UI update
 }
 
