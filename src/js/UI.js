b55de0d83cec3f72f93ea2b6ddf763592a1d6a9d
christian@baltzer.de <christian@baltzer.de>
2019-10-01 10:18:04 +0200

    builded a new UI

--- src/js/UI.js
+++ src/js/UI.js
@@ -1,28 +1,64 @@
-var box = document.body
+import * as connection from 'data_connection.js'
 
-export function build_name(params) {
-  // form with UserInput
-  var input = document.createElement('input')
-
-  input.type = 'radio'
-  input.name = 'question'
-  input.placeholder = 'Your Name?'
-
-  input.addEventListener('input', function () {
-    let text = input.value
-
-  })
+export function view () {
+  const temp = document.createElement('artikel')
+  return temp
 }
 
-export function build_Menü(params) {
-  // Welcome, button_start, bord
+export function button (name) {
+  const temp = document.createElement('button')
+  temp.name = name
+  return temp
 }
 
-export function build_quiz(question, answer) {
-  // wenn mehrere antworten möglich sind, bauen wir antwortmöglichkeiten
-  // wenn nur eine nich
+export function label (text) {
+  const temp = document.createElement('p')
+  temp.innerText = text
+  return temp
 }
 
-export function reset() {
-  box = ''
+export function input (name) {
+  const temp = document.createElement('input')
+  temp.type = 'text'
+  temp.placeholder = name
+  return temp
+}
+
+export function multipleChoice (question, answers) {
+  const arr = answers
+  const temp = document.createElement('select')
+
+  for (let i = 0; i < arr.length; i++) {
+    const input = document.createElement('option')
+    input.value = arr[i]
+    input.text = arr[i]
+
+    temp.appendChild(input)
+  }
+  return temp
+}
+
+export function timer_New (time) {
+  // TODO: Timer UI
+}
+
+export function timer_Update (time) {
+  // TODO: Timer UI update
+}
+
+export function scoreboard () {
+  let arr = connection.Cookie_Load()
+  arr = []
+  const list = document.createElement('li')
+
+  for (let i = 0; i < arr.length; i++) {
+    var temp = document.createElement('ol') 
+    temp.innerText = arr[i].name + '      ' + arr[i].points
+  }
+  list.id = 'scoreboard'
+  return list
+}
+
+export function reset () {
+  this.innerhtml = ''
 }
