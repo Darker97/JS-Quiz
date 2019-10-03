dfee7e22f90ff34a8a25d92953402b768938c5ab
christian@baltzer.de <christian@baltzer.de>
2019-10-03 16:42:05 +0200

    rebuild the fetch funktion and fisnished the game

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 import * as UI from './UI.js'
 import * as person from './Person.js'
-import * as connect from './data_Connection.js'
+import { cookieSave } from './Data_Save.js'
+import { Timer, TimerStop } from './timer.js'
 
 /* -------------------------- */
 var Workingobjekt = UI.view()
 var User
+var questionObjekt = new Object()
 /* -------------------------- */
 
 /* -------------------------- */
@@ -54,6 +56,7 @@ function Phase2 () {
   Workingobjekt.appendChild(UI.scoreboard())
 
   button.addEventListener('click', function () {
+    questionObjekt.nextURL = 'http://vhost3.lnu.se:20080/question/1'
     Phase3()
   })
 }
@@ -63,9 +66,7 @@ function Phase2 () {
  */
 
 function Phase3 () {
-  let points = 0
-  let questionObjekt = new Object()
-  questionObjekt.nextURL = 'http://vhost3.lnu.se:20080/question/1'
+  // -------------------------------------
   // reset Page
   Workingobjekt.innerHTML = ''
   fetch(questionObjekt.nextURL)
@@ -75,13 +76,25 @@ function Phase3 () {
       questionObjekt = resp
     })
     .then(function () {
-      if ('answers' in questionObjekt !== true) {
+      // Just a simple question?
+
+      // Start the Timer
+      let temp = UI.timerNew('20')
+      Workingobjekt.appendChild(temp)
+      Timer(temp)
+
+      if ('alternatives' in questionObjekt !== true) {
+        // Build the Webpage
         Workingobjekt.appendChild(UI.label(questionObjekt.question))
         let input = UI.input('Your answer')
         Workingobjekt.appendChild(input)
+        let button = UI.button('send')
+        Workingobjekt.appendChild(button)
 
-        input.addEventListener('click', function () {
-        // ___________________________ => start of answer
+        // Button is clicked, we continue
+        button.addEventListener('click', function () {
+
+          // ___________________________ => start of answer
           let temp = new Object()
           temp.answer = input.value
 
@@ -96,25 +109,119 @@ function Phase3 () {
             .then((resp) => resp.json())
             .then(function (resp) {
               console.log(resp)
+
+              // answer is wrong, continue to finishedFail
+              if (resp.message === 'Wrong answer! :(') {
+                finishedFail()
+                return
+              }
+              // answer is last, continue to finishedSucces
+              if (resp.message === 'Correct answer!' && typeof resp.nextURL === 'undefined') {
+                finishedSucces()
+                return
+              }
+              questionObjekt = resp
+              Phase3()
             })
         })
-      // ___________________________ => end of answer
       } else {
-        let input = UI.multipleChoice(questionObjekt.question, questionObjekt.alternatives)
+        // Multiple choice
+        Workingobjekt.appendChild(UI.label(questionObjekt.question))
+        let input = UI.multipleChoice(questionObjekt.alternatives)
 
         Workingobjekt.appendChild(input)
-        input.addEventListener('click', function () {
-          // TODO: WHAT TO DO WHEN THIS WORKS?
+        let button = UI.button('send')
+        Workingobjekt.appendChild(button)
+
+        // Button is clicked, we continue
+        button.addEventListener('click', function () {
+
+          // ___________________________ => start of answer
+          let temp = new Object()
+          temp.answer = input.value
+
+          let fetchData = {
+            method: 'POST',
+            body: JSON.stringify(temp),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+          fetch(questionObjekt.nextURL, fetchData)
+            .then((resp) => resp.json())
+            .then(function (resp) {
+              console.log(resp)
+
+              // answer is wrong, continue to finishedFail
+              if (resp.message === 'Wrong answer! :(') {
+                Timer.TimerStop()
+                finishedFail()
+                return
+              }
+              // answer is last, continue to finishedSucces
+              if (resp.message === 'Correct answer!' && typeof resp.nextURL === 'undefined') {
+                finishedSucces()
+                return
+              }
+              questionObjekt = resp
+              Phase3()
+            })
         })
       }
     })
-  // End of the Game
-  Phase4()
+    // ___________________________ => end of answer
 }
 
 /**
  * Game Over
  * Return to Phase 2 or 3 for a new try
  */
-function Phase4 () {
+export function finishedFail () {
+  // reset Page
+  Workingobjekt.innerHTML = ''
+  Workingobjekt.appendChild(UI.label('Wrong answer 🙁'))
+  Workingobjekt.appendChild(UI.label('better Luck next time 🍀'))
+  Workingobjekt.appendChild(UI.label('Wanna play again?'))
+  let buttonPlay = UI.button('Play again 😎')
+  let buttonMenu = UI.button('Main Menu 🖥')
+
+  Workingobjekt.appendChild(buttonPlay)
+  Workingobjekt.appendChild(buttonMenu)
+
+  buttonPlay.addEventListener('click', function () {
+    User.score = 0
+    questionObjekt.nextURL = 'http://vhost3.lnu.se:20080/question/1'
+    Phase3()
+  })
+  buttonMenu.addEventListener('click', function () {
+    User.score = 0
+    Phase2()
+  })
+}
+/**
+ * Game End
+ * score is saved and you can play the next game
+ */
+function finishedSucces() {
+  // reset Page
+  Workingobjekt.innerHTML = ''
+  Workingobjekt.appendChild(UI.headline('you did it 🥳'))
+  Workingobjekt.appendChild(UI.label('Your score is: ' + User.score))
+  cookieSave(User.name, User.score)
+  Workingobjekt.appendChild(UI.label('Wanna play again?'))
+  let buttonPlay = UI.button('Play again 😎')
+  let buttonMenu = UI.button('Main Menu 🖥')
+
+  Workingobjekt.appendChild(buttonPlay)
+  Workingobjekt.appendChild(buttonMenu)
+
+  buttonPlay.addEventListener('click', function () {
+    User.score = 0
+    questionObjekt.nextURL = 'http://vhost3.lnu.se:20080/question/1'
+    Phase3()
+  })
+  buttonMenu.addEventListener('click', function () {
+    User.score = 0
+    Phase2()
+  })
 }
